a7f4768b3d0304854c4cc00dc24eb476816517fb
Smeagle <therealhbfv@gmail.com>
2017-09-25 12:29:20 +0100

    Add files via upload

--- FightGame.py
+++ FightGame.py
@@ -1,4 +1,4 @@
-#V0.151
+#V0.2
 import random
 import tkinter
 
@@ -23,11 +23,11 @@ def wrestler():
 menu=tkinter.Tk()
 menu.title("Fight Game - Menu")
 menu.geometry("300x300")
-menu.configure(background="#7F0404")
-title=tkinter.Label(menu, text="Fight Game", font=("Courier", 30), pady=20,bg="#7F0404", fg="white")
-boxer=tkinter.Button(menu, text="Boxer", command=boxer, pady=10, padx=10)
-kickboxer=tkinter.Button(menu, text="Kick Boxer",command=kickboxer, pady=20, padx=10)
-wrestler=tkinter.Button(menu, text="Wrestler",command=wrestler, pady=20, padx=10)
+menu.configure(background="#7F0404", cursor="dot")
+title=tkinter.Label(menu, text="Fight Game", font=("Courier", 28, "bold"), pady=20,bg="#7F0404", fg="white")
+boxer=tkinter.Button(menu, text="Boxer", command=boxer, pady=20, width=100)
+kickboxer=tkinter.Button(menu, text="Kick Boxer",command=kickboxer, pady=20, width=100)
+wrestler=tkinter.Button(menu, text="Wrestler",command=wrestler, pady=20, width=100)
 title.pack()
 boxer.pack()
 kickboxer.pack()
@@ -42,7 +42,7 @@ def gameplay():
     window=tkinter.Tk()
     window.title("Fight Game - Playing")
     window.geometry("640x480")
-    window.configure(background="#7F0404")
+    window.configure(background="#7F0404", cursor="dot")
     global health
     global cclass
     if cclass=="boxer":
@@ -62,9 +62,9 @@ def gameplay():
         health=175
 
     global ehealth
-    ehealth=150
+    ehealth=health*1.25
     global emodifier
-    emodifier=1.25
+    emodifier=1.4
 
     global pwin
     pwin=0
@@ -87,27 +87,33 @@ def gameplay():
         global pwin
         if pwin!=1:
             global health
-            echance=random.randint(0,10)
-            if echance>3:
+            echance=random.randint(0,13)
+            if echance>=5:
                 global ehealth
                 health-=random.randint(10,20)*emodifier
                 print("Player health is now: "+str(health))
                 healthl.configure(text="Health: "+str(health))
-            elif echance<=3:
+            elif echance<=4:
                 chance=1
                 while chance<7:
                     health-=random.randint(2,7)*emodifier
                     chance=random.randint(0,11)
                     print("Player health is now: "+str(health))
                     healthl.configure(text="Health: "+str(health))
+            elif echance>=11:
+                print("Enemy attack missed!")
 
     def punchp():
         global pwin
         if pwin!=1:
             global ehealth
-            ehealth-=random.randint(10,20)*pmodifier
-            print("Enemy health is now: "+str(ehealth))
-            ehealthl.configure(text="Health: "+str(ehealth))
+            missch=random.randint(0,13)
+            if missch>=11:
+                print("Player attack missed!")
+            else:
+                ehealth-=random.randint(10,20)*pmodifier
+                print("Enemy health is now: "+str(ehealth))
+                ehealthl.configure(text="Health: "+str(ehealth))
             pwin()
             echance()
             ewin()
@@ -117,9 +123,13 @@ def gameplay():
         global pwin
         if pwin!=1:
             global ehealth
-            ehealth-=random.randint(10,20)*kmodifier
-            print("Enemy health is now: "+str(ehealth))
-            ehealthl.configure(text="Health: "+str(ehealth))
+            missch=random.randint(0,13)
+            if missch>=11:
+                print("Player attack missed!")
+            else:    
+                ehealth-=random.randint(10,20)*kmodifier
+                print("Enemy health is now: "+str(ehealth))
+                ehealthl.configure(text="Health: "+str(ehealth))
             pwin()
             echance()
             ewin()
@@ -128,32 +138,33 @@ def gameplay():
         if pwin!=1:
             global ehealth
             chance=1
-            while chance<7:
-                ehealth-=random.randint(2,7)*gmodifier
-                chance=random.randint(0,11)
-                print("Enemy health is now: "+str(ehealth))
-                ehealthl.configure(text="Health: "+str(ehealth))
+            missch=random.randint(0,13)
+            if missch>=11:
+                while chance<7:
+                    ehealth-=random.randint(2,7)*gmodifier
+                    chance=random.randint(0,11)
+                    print("Enemy health is now: "+str(ehealth))
+                    ehealthl.configure(text="Health: "+str(ehealth))
             pwin()
             echance()
             ewin()
 		
-		
-    player=tkinter.Label(window, text="Player")
-    healthl=tkinter.Label(window, text="Health: "+str(health))
-    enemy=tkinter.Label(window, text="Enemy")
-    ehealthl=tkinter.Label(window, text="Health: "+str(ehealth))
+    player=tkinter.Label(window, text="Player", font=("Courier", 24, "bold"))
+    healthl=tkinter.Label(window, text=("Health: "+str(health)), font=("Courier", 18))
+    enemy=tkinter.Label(window, text="Enemy", font=("Courier", 24, "bold"))
+    ehealthl=tkinter.Label(window, text=("Health: "+str(ehealth)), font=("Courier", 18))
     punch=tkinter.Button(window, text="Punch",command=punchp)
     kick=tkinter.Button(window, text="Kick", command =kickp)
     grapple=tkinter.Button(window, text="Grapple", command=grapplep)
 
 ###############################################
 #LOAD INTO GAME WINDOW
-    player.pack(pady=10, padx=20)
+    player.pack(pady=10, padx=20, fill=tkinter.X)
     healthl.pack()
-    punch.pack(pady=15)
-    kick.pack(pady=15)
-    grapple.pack(pady=15)
-    enemy.pack(pady=10)
+    punch.pack(pady=5)
+    kick.pack(pady=5)
+    grapple.pack(pady=5)
+    enemy.pack(pady=10, padx=20, fill=tkinter.X)
     ehealthl.pack()
 
 ###############################################
